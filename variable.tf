62ecb42120d2bd529db8e28d44aa152c600398c7
naveenpermi <8433naveen@gmail.com>
2025-09-11 13:13:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,7 @@
 variable "filename" {
-  description = "The name of the file to create"
-  type        = string
+ 
 }
 
 variable "content" {
-  description = "The content to put inside the file"
-  type        = string
+ 
 }
