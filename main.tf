1aae15900e1e8612d2be9bdc34086d30df8896f2
naveenpermi <8433naveen@gmail.com>
2025-09-12 16:00:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
-resource "local_file" "my_pet" {
-  filename = var.filename
-  content  = var.content
+resource "aws_iam_user" "user" {
+  name = var.username
+  tags = {
+    Description = "Created by Jenkins/Terraform"
+  }
 }
 
-resource "random_pet" "petname" {
-  prefix    = "Mr"
-  separator = "."
-  length    = "1"
+resource "aws_iam_policy" "admin_policy" {
+  name   = "${var.username}-policy"
+  policy = file("${path.module}/admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "user_attach" {
+  user       = aws_iam_user.user.name
+  policy_arn = aws_iam_policy.admin_policy.arn
+}
+
+output "iam_username" {
+  value = aws_iam_user.user.name
 }
